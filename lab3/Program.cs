daade509d9821605fe33ee585203c3f19b332e91
Дмитрий <dimaserr5@gmail.com>
2022-09-14 16:20:50 +0700

    Выборка задачи

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+internal class KPP_lab3_zd1
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Выберите приложение:");
+        Console.WriteLine("1 - Однозначное");
+        Console.WriteLine("2 - ДвухЗначное");
+        Console.WriteLine("3 - Трёхзначное");
+        Console.WriteLine("4 - Треугольник");
+
+        string zadacha = Console.ReadLine();
+
+        switch (zadacha) {
+
+            case "1":
+                Console.WriteLine("1");
+            break;
+
+            case "2":
+                Console.WriteLine("2");
+            break;
+
+            case "3":
+                Console.WriteLine("3");
+            break;
+
+            case "4":
+                Console.WriteLine("4");
+            break;
+
+
+            default:
+                Console.WriteLine("Ошибка задачи");
+            break;
+            
+
+        }
+            
+
+    }
+}
